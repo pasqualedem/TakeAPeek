61825b1e5778a4c57427d2105c3b7a78b304b4d5
pasqualedem <pasquale199876@hotmail.it>
2025-05-04 19:36:55 +0200

    removes useless files

--- main.py
+++ main.py
@@ -13,52 +13,6 @@ from tap.logger.text_logger import get_logger
 logger = get_logger(__name__)
 
 
-class ParallelRun:
-    slurm_command = "sbatch"
-    slurm_script = "slurm/launch_lora"
-    slurm_multi_gpu_script = "slurm/launch_lora_multi_gpu"
-    slurm_script_first_parameter = "--parameters="
-    slurm_outfolder = "out"
-    out_extension = "out"
-    param_extension = "yaml"
-    slurm_stderr = "-e"
-    slurm_stdout = "-o"
-
-    def __init__(self, params: dict, experiment_timestamp: str, multi_gpu=False, only_create=False):
-        self.params = params
-        self.exp_timestamp = experiment_timestamp
-        self.multi_gpu = multi_gpu
-        if "." not in sys.path:
-            sys.path.extend(".")
-
-    def launch(self, only_create=False):
-        subfolder = f"{self.exp_timestamp}_{self.params['experiment']['group']}"
-        out_folder = os.path.join(self.slurm_outfolder, subfolder)
-        os.makedirs(out_folder, exist_ok=True)
-
-        run_uuid = str(uuid.uuid4())[:8]
-        out_file = f"{run_uuid}.{self.out_extension}"
-        out_file = os.path.join(out_folder, out_file)
-        param_file = f"{run_uuid}.{self.param_extension}"
-        param_file = os.path.join(out_folder, param_file)
-        write_yaml(self.params, param_file)
-        slurm_script = self.slurm_multi_gpu_script if self.multi_gpu else self.slurm_script
-        command = [
-            self.slurm_command,
-            self.slurm_stdout,
-            out_file,
-            self.slurm_stderr,
-            out_file,
-            slurm_script,
-            self.slurm_script_first_parameter + param_file,
-        ]
-        if only_create:
-            logger.info(f"Creating command: {' '.join(command)}")
-        else:
-            logger.info(f"Launching command: {' '.join(command)}")
-            subprocess.run(command)
-
-
 def experiment_lora(param_file, multi_gpu=False, only_create=False, sequential=False):
     timestamp = get_timestamp()
     settings = load_yaml(param_file)
@@ -103,9 +57,7 @@ def experiment_lora(param_file, multi_gpu=False, only_create=False, sequential=F
                     run_params["target_modules"] = [mod for mod in run_params["target_modules"] if len(mod) > 0]
                 main(copy.deepcopy(run_params))
             else:
-                run_params['experiment'] = settings["experiment"]
-                run = ParallelRun(run_params, experiment_timestamp=timestamp, multi_gpu=multi_gpu, only_create=only_create)
-                run.launch()
+                raise NotImplementedError("Parallel runs are not implemented yet.")
 
 
 @click.command()
